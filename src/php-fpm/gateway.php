f21e1030afc489d4260657db27e1d02df6b2aff7
tuuna <b7712983@naver.com>
2026-09-24 19:34:26 +0900

    관측 코드 추가

--- src/php-fpm/gateway.php
+++ src/php-fpm/gateway.php
@@ -1,3 +1,14 @@
-<?php
+<?php 
 
-echo "Hello World\n";
+require 'vendor/autoload.php';
+
+class DemoClass
+{
+    public function run(): void
+    {
+        echo "Hello Tuuna\n";
+    }
+}
+
+$demo = new DemoClass();
+$demo->run();
---
+++ src/php-fpm/telemetry.php
@@ -0,0 +1,33 @@
+<?php
+
+use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\StatusCode;
+use OpenTelemetry\Context\Context;
+
+require __DIR__ . '/vendor/autoload.php';
+
+OpenTelemetry\Instrumentation\hook(
+    class: DemoClass::class,
+    function: 'run',
+    pre: static function (DemoClass $demo, array $params, string $class, string $function, ?string $filename, ?int $lineno) {
+        static $instrumentation;
+        $instrumentation ??= new CachedInstrumentation('example');
+        $span = $instrumentation->tracer()->spanBuilder('democlass-run')->startSpan();
+        Context::storage()->attach($span->storeInContext(Context::getCurrent()));
+    },
+    post: static function (DemoClass $demo, array $params, $returnValue, ?Throwable $exception) {
+        $scope = Context::storage()->scope();
+        if (null === $scope) {
+            return;
+        }
+        $scope->detach();
+        $span = Span::fromContext($scope->context());
+        if ($exception) {
+            $span->recordException($exception);
+            $span->setStatus(StatusCode::STATUS_ERROR);
+        }
+        $span->end();
+    }
+);
+
